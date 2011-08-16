de44cdea3da9d2807cc3ca330a6b575370c4a5bb
Ian Terrell <ian.terrell@gmail.com>
2011-08-16 11:15:50 -0400

    Initial Commit

---
+++ ExampleEngine/EEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EEAppDelegate.h
+//  ExampleEngine
+//
+//  Created by Ian Terrell on 8/16/11.
+//  Copyright (c) 2011 Ian Terrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ExampleEngine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExampleEngine
+//
+//  Created by Ian Terrell on 8/16/11.
+//  Copyright (c) 2011 Ian Terrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([EEAppDelegate class]));
+  }
+}
