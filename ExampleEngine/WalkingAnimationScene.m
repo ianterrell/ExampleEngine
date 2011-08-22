73b5cd7522dde628a6c96a792d7f536a39f01f5a
Ian Terrell <ian.terrell@gmail.com>
2011-08-22 11:50:41 -0400

    walking across screen example

--- ExampleEngine/WalkingAnimationScene.m
+++ ExampleEngine/WalkingAnimationScene.m
@@ -16,10 +16,11 @@
   self = [super init];
   if (self) {
     EESprite *sprite = [[EESprite alloc] initWithImage:[UIImage imageNamed:@"alfred0.png"] pointRatio:100];
-//    sprite.position = GLKVector2Make(3,0);
-    
     NSArray *frameNames = [NSArray arrayWithObjects:@"alfred1.png", @"alfred2.png", @"alfred3.png", @"alfred4.png", @"alfred5.png", @"alfred6.png", @"alfred7.png", @"alfred8.png", @"alfred9.png", nil];
     sprite.spriteAnimation = [[EESpriteAnimation alloc] initWithTimePerFrame:1.0/9 framesNamed:frameNames];
+    
+    sprite.position = GLKVector2Make(3, 0);
+    sprite.velocity = GLKVector2Make(-1, 0);
     
     [self.shapes addObject:sprite];
   }
